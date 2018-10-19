cf1e68bae6873be4c1ce4d8805aaddc870759570
youxkei <youxkei@gmail.com>
2018-10-19 21:46:24 +0900

    [issue-16] Implement derivation of Let and Letrec

--- lib/derivation.ml
+++ lib/derivation.ml
@@ -1,11 +1,12 @@
 open Base
 open Ast_intf
+open Result
 
-let new_var =
-  let i = ref 0 in
-  fun () ->
-    Int.incr i;
-    Printf.sprintf "%s%02d" "v" !i
+let tyvar_count = ref 0
+
+let new_tyvar () =
+  Int.incr tyvar_count;
+  TyVar (Printf.sprintf "%s%02d" "v" !tyvar_count)
 
 let rec derive context = function
   | Val c ->
@@ -13,30 +14,57 @@ let rec derive context = function
   | Var v ->
      begin match Context.find context v with
      | Some ty ->
-        Ok(ty, Empty)
+        Ok (ty, Empty)
      | None ->
         Error ("unknown type variable: " ^ v)
      end
+  (* TODO: implement derivation of application expression with multiple arguments *)
   | App (f, [arg1]) ->
-     let open Result in
      derive context f >>= fun (tyf, cf) ->
      derive context arg1 >>= fun (ty1, c1) ->
-     let alpha1 = new_var() in
-     let alpha = new_var() in
-     let beta = new_var() in
+     let alpha1 = new_tyvar () in
+     let alpha = new_tyvar () in
+     let beta = new_tyvar () in
      let constraints = [
-         Eq (tyf, TyFun ([TyVar alpha1], TyVar alpha));
-         Subtype (TyVar beta, TyVar alpha);
-         Subtype (ty1, TyVar alpha1);
+         Eq (tyf, TyFun ([alpha1], alpha));
+         Subtype (beta, alpha);
+         Subtype (ty1, alpha1);
          cf;
          c1;
        ]
      in
-     Ok(TyVar beta, Conj constraints)
-  | Abs ([v], e) ->
-     let open Result in
-     let ty_v = new_var () in
-     derive (Context.add v (TyVar ty_v) context) e >>= fun (ty_e, c) ->
-     Ok(TyFun([TyVar ty_v], TyConstraint(ty_e, c)), Empty)
+     Ok (beta, Conj constraints)
+  | Abs (vs, e) ->
+     let new_tyvars = List.map ~f:(fun v -> (v, (new_tyvar ()))) vs in
+     let added_context =
+       List.fold_left
+         ~f:(fun context (v, tyvar) -> Context.add v tyvar context)
+         ~init:context
+         new_tyvars in
+     derive added_context e >>= fun (ty_e, c) ->
+     let tyvar = new_tyvar () in
+     Ok (tyvar, Eq (tyvar, TyConstraint (TyFun (List.map ~f:snd new_tyvars, ty_e), c)))
+  | Let (v, e1, e2) ->
+     derive context e1 >>= fun (ty_e1, c1) ->
+     derive (Context.add v ty_e1 context) e2 >>= fun (ty_e2, c2) ->
+     Ok (ty_e2, Conj [c1; c2])
+  | Letrec (lets , e) ->
+     let new_tyvars = List.map ~f:(fun (v, f) -> (v, f, (new_tyvar ()))) lets in
+     let added_context =
+       List.fold_left
+         ~f:(fun context (v, _, tyvar) -> Context.add v tyvar context)
+         ~init:context
+         new_tyvars in
+     let constraints_result =
+       List.fold_left
+         ~f:(fun constraints_result (v, f, tyvar) ->
+               constraints_result >>= fun constraints ->
+               derive added_context f >>= fun (ty, c) ->
+               Ok (Eq (tyvar, ty) :: c :: constraints))
+         ~init:(Ok [])
+         new_tyvars in
+     constraints_result >>= fun constraints ->
+     derive added_context e >>= fun (ty, c) ->
+     Ok (ty, Conj (c :: constraints))
   | other ->
      Error (Printf.sprintf "unsupported type: %s" (show_expr other))
