c476e36ae0ca633d24a73a71a104abca8138ad1c
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2018-10-19 19:41:41 +0900

    introduce type substitute function

--- lib/solver.ml
+++ lib/solver.ml
@@ -4,7 +4,28 @@ open Ast_intf
 type ty_var = string
 type sol = (ty_var, typ, String.comparator_witness) Map.t
 
-let solve_eq sol ty1 ty2 = Ok sol (*TODO : see TAPL https://dwango.slack.com/messages/CD453S78B/ *)
+(* 型 type_subst (X, τ_1) τ_2 := [X ↦ τ_1]τ_2 *)
+let rec type_subst ((x : ty_var) , (ty1 : typ)): typ -> typ = function
+  | TyStruct tys ->
+     TyStruct(List.map ~f:(type_subst (x, ty1)) tys)
+  | TyFun (tys, ty) ->
+     TyFun (List.map ~f:(type_subst (x, ty1)) tys, type_subst (x, ty1) ty)
+  | TyUnion (ty_a, ty_b) ->
+     TyUnion(type_subst (x, ty1) ty_a, type_subst (x, ty1) ty_b)
+  | TyConstraint (ty, c) ->
+     TyConstraint(type_subst (x, ty1) ty, type_subst_to_constraint (x, ty1) c)
+  | TyAny -> TyAny
+  | TyNone -> TyNone
+  | TyInteger -> TyInteger
+  | TyAtom -> TyAtom
+  | TyConstant const -> TyConstant const
+  | TyVar (v:ty_var) ->
+     if
+       String.equal x v
+     then ty1 else (TyVar v)
+and type_subst_to_constraint (x, ty1) = function
+  | _ ->
+     failwith "not implemented "
 let solve_sub sol ty1 ty2 = Ok sol (*TODO*)
 
 let rec solve sol = function
@@ -16,7 +37,7 @@ let rec solve sol = function
   | Conj cs ->
      solve_conj sol cs
   | Disj cs ->
-     Error (Failure "unimplemented : solving Disjunction of constraints")
+     Error (Failure "not implemented : solving Disjunction of constraints")
 and solve_conj sol = function
   | [] -> Ok sol
   | c :: cs ->
