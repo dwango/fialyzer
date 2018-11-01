adb9c5a1785099fb84c0952fc33ca9710f82efe6
amutake <amutake.s@gmail.com>
2018-11-01 17:31:28 +0900

    Export some functions in Solver module

--- lib/solver.mli
+++ lib/solver.mli
@@ -1,9 +1,13 @@
 open Ast_intf
 
 type solution
+[@@deriving sexp_of]
 
 val string_of_sol : solution -> string
 
 val init : solution
 
 val solve : solution -> constraint_ -> (solution, exn) result
+
+(* TODO: don't export *)
+val meet : solution -> typ -> typ -> typ
