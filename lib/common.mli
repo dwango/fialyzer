b85c957cc3d3c747a27138e7f942105c24af0b83
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2018-11-09 23:42:30 +0900

    [issue-43] add result operation

--- lib/common.mli
+++ lib/common.mli
@@ -1,6 +1,10 @@
 open Base
 
 val result_map_m : 'a list -> f:('a -> ('b, 'c) Result.t) -> ('b list, 'c) Result.t
+val result_guard : bool -> 'err -> (unit, 'err) Result.t
+val result_or : ('a, 'e) Result.t -> ('a, 'e) Result.t -> ('a, 'e) Result.t
+val (@?) : ('a, exn) Result.t -> string -> ('a, exn) Result.t
+
 val (!%) : ('a, unit, string) format -> 'a
 val (<<<) : ('b -> 'c) -> ('a -> 'b) -> 'a -> 'c
 val (>>>) : ('a -> 'b) -> ('b -> 'c) -> 'a -> 'c
