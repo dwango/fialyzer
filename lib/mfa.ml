b6a666dfe37eb10c1242748ae29807b8ea0ae1e3
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2018-11-09 23:42:05 +0900

    [issue-43] showable mfa

--- lib/mfa.ml
+++ lib/mfa.ml
@@ -2,5 +2,6 @@
    The tuple of module_name, function_name and arity
  *)
 open Base
+module Format = Caml.Format
 
-type t = string * string * int [@@deriving sexp_of]
+type t = string * string * int [@@deriving show, sexp_of]
