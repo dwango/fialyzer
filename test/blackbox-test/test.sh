9d29eb272202a7e573a1efd1d5b3e4b9b1638d01
amutake <amutake.s@gmail.com>
2018-11-14 14:38:26 +0900

    Add template for blackbox tests

---
+++ test/blackbox-test/test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -eu
+cd $(dirname $0)
+
+fialyzer=$1
+dir="test-cases"
+
+for beam in $(ls ${dir}/*.beam); do
+  testname=$(basename $beam .beam)
+  expected="${dir}/${testname}.expected"
+  output="${dir}/${testname}.output"
+  $fialyzer $beam > $output
+  diff $output $expected
+done
