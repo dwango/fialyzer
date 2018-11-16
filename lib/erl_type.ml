a7743270b19494a8a159cd8fe1ddeed1d83386d0
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2018-11-16 13:39:57 +0900

    [issue-43] move erl_type_of_etf to Erl_type.of_etf

--- lib/erl_type.ml
+++ lib/erl_type.ml
@@ -32,6 +32,34 @@ let mandatoriness_of_etf = function
   | Atom "opt" -> Ok Optional
   | _ -> Error (Failure "mandatoriness_of_etf")
 
+type tag = AtomTag | BinaryTag | FunctionTag | IdentifierTag | ListTag | MapTag
+           | MatchstateTag | NilTag | NumberTag | OpaqueTag | ProductTag
+           | TupleTag | TupleSetTag | UnionTag | VarTag
+[@@deriving show, sexp_of]
+
+let tag_of_etf etf =
+  let open Result in
+  let tag_of_atom = function
+    | "atom" -> Ok AtomTag
+    | "binary" -> Ok BinaryTag
+    | "function" -> Ok FunctionTag
+    | "identifier" -> Ok IdentifierTag
+    | "list" -> Ok ListTag
+    | "map" -> Ok MapTag
+    | "matchstate" -> Ok MatchstateTag
+    | "nil" -> Ok NilTag
+    | "number" -> Ok NumberTag
+    | "opaque" -> Ok OpaqueTag
+    | "product" -> Ok ProductTag
+    | "tuple_set" -> Ok TupleSetTag
+    | "tuple" -> Ok TupleTag
+    | "union" -> Ok UnionTag
+    | "var" -> Ok VarTag
+    | other -> Error (Failure(!%"tag_of_atom: unknown : %s" other))
+  in
+  E.atom_of_etf etf >>= fun atom ->
+  tag_of_atom atom
+
 (**
 {v
 -record(c, {tag			      :: tag(),
@@ -71,3 +99,106 @@ and opaque = {
     struct_ : t;
   }
 [@@deriving show, sexp_of]
+
+let rec of_etf = function
+  | Etf.Atom "any" -> Ok Any
+  | Atom "none" -> Ok None
+  | Atom "unit" -> Ok Unit
+  | SmallTuple(4, [
+                 Etf.Atom "c";
+                 tag_etf;
+                 elements;
+                 qualifier_etf;
+              ]) ->
+     let open Result in
+     tag_of_etf tag_etf >>= fun tag ->
+     begin match tag with
+     | AtomTag ->
+        E.list_of_etf elements >>= fun elems ->
+        result_map_m ~f:E.atom_of_etf elems >>= fun atoms ->
+        Ok (Atom atoms)
+     | FunctionTag ->
+        E.list_of_etf elements >>= fun elems ->
+        result_guard (List.length elems = 2) (Failure "erl_type_of_erl:FunctionTag") >>= fun _ ->
+        let domain_etf = List.nth_exn elems 0 in
+        let range_etf = List.nth_exn elems 1 in
+        of_etf domain_etf >>= fun domain_ty ->
+        begin match domain_ty with
+        | Product domain ->
+           of_etf range_etf >>= fun range ->
+           Ok (Function(domain, range))
+        | other ->
+           Error(Failure (!%"tyfun_of_etf: unsupported"))
+        end
+     | ListTag ->
+        E.list_of_etf elements >>= fun elems ->
+        begin match elems with
+        | [types_etf; term_etf] ->
+           of_etf types_etf >>= fun types ->
+           of_etf term_etf >>= fun term ->
+           (qualifier_of_etf qualifier_etf @? "ListTag") >>= fun size ->
+           Ok (List (types, term, size))
+        | _ ->
+           Error (Failure (!%"ListTag:%s" (Etf.show elements)))
+        end
+     | NilTag ->
+        Ok Nil
+     | MapTag ->
+        E.tuple_of_etf elements >>= fun elems ->
+        begin match elems with
+        | [pairs_etf; defkey_etf; defval_etf] ->
+           of_etf defkey_etf >>= fun defkey ->
+           of_etf defval_etf >>= fun defval ->
+           E.list_of_etf pairs_etf >>= fun pair_etfs ->
+           result_map_m ~f:t_map_pair_of_etf pair_etfs >>= fun t_map_dict ->
+           Ok (Map (t_map_dict, defkey, defval))
+        | _ ->
+           Error (Failure "erl_types(MapTag)")
+        end
+     | OpaqueTag ->
+        E.list_of_etf elements >>= fun elems ->
+        result_map_m ~f:opaque_of_etf elems >>= fun opaques ->
+        Ok (Opaque opaques)
+     | ProductTag ->
+        E.list_of_etf elements >>= fun elems ->
+        result_map_m ~f:of_etf elems >>= fun tys ->
+        Ok (Product tys)
+     | TupleTag ->
+        E.list_of_etf elements >>= fun elems ->
+        result_map_m ~f:of_etf elems >>= fun tys ->
+        E.pair_of_etf qualifier_etf >>= fun (arity_etf, tag_etf) ->
+        E.int_of_etf arity_etf >>= fun arity ->
+        (of_etf tag_etf @? !%"TupleTag(%s)" (Etf.show tag_etf)) >>= fun tag ->
+        Ok (Tuple(tys, arity, tag))
+     | UnionTag ->
+        E.list_of_etf elements >>= fun elems ->
+        result_map_m ~f:of_etf elems >>= fun tys ->
+        Ok (Union tys)
+     | other ->
+        !%"of_etf: unsupported: %s\n  elements = %s" (show_tag other)
+          (Etf.show elements)
+        |> fun msg -> Error (Failure msg)
+     end
+  | other ->
+     Error (Failure (!%"of_etf error: %s" (Etf.show other)))
+and opaque_of_etf elem =
+  let open Result in
+  E.tuple_of_etf elem >>= function
+  | [Atom "opaque"; Atom mod_; Atom name; args_etf; struct_etf] ->
+     E.list_of_etf args_etf >>= fun arg_etfs ->
+     result_map_m ~f:of_etf arg_etfs >>= fun args ->
+     of_etf struct_etf >>= fun struct_ ->
+     Ok {mod_; name; args; struct_}
+  | other ->
+     Error (Failure "opaque_of_etf")
+and t_map_pair_of_etf etf =
+  let open Result in
+  E.tuple_of_etf etf >>= fun es ->
+  match es with
+  | [ty_etf1; mand_etf; ty_etf2] ->
+     of_etf ty_etf1 >>= fun ty1 ->
+     of_etf ty_etf2 >>= fun ty2 ->
+     mandatoriness_of_etf mand_etf >>= fun mand ->
+     Ok (ty1, mand, ty2)
+  | _ ->
+     Error (Failure "t_map_pair_of_etf")
