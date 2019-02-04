e4aebfddbb80e8de1dcacf8cf599f0ad7abba308
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2019-02-04 18:46:05 +0900

    [issue-182] fix test

--- test/unit-test/test_solver.ml
+++ test/unit-test/test_solver.ml
@@ -8,10 +8,12 @@ let sexp_of_solution sol =
   |> [%sexp_of: string Map.M(Type_variable).t]
 
 let eq (ty1, ty2) =
-  Constraint.Eq {lhs=ty1; rhs=ty2}
+  let expr = Ast.Var (-1, "__dummy_expr__") in
+  Constraint.Eq {lhs=ty1; rhs=ty2; expr}
 
 let subtype (ty1, ty2) =
-  Constraint.Subtype {lhs=ty1; rhs=ty2}
+  let expr = Ast.Var (-1, "__dummy_expr__") in
+  Constraint.Subtype {lhs=ty1; rhs=ty2; expr}
 
 let%expect_test "solver" =
   let print c =
