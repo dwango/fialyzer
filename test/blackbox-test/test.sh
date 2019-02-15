59690b6a05e6c3c264f29bd37c17bbee5161920b
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2019-02-15 18:59:17 +0900

    using plt file in blackbox testings

--- test/blackbox-test/test.sh
+++ test/blackbox-test/test.sh
@@ -4,12 +4,13 @@ set -u
 cd $(dirname $0)
 
 fialyzer=$1
+plt=$2
 dir="test-cases"
 
 for beam in $(ls ${dir}/*.beam); do
   testname=$(basename $beam .beam)
   expected="${dir}/${testname}.expected"
   output="${dir}/${testname}.output"
-  $fialyzer $beam &> $output
+  $fialyzer --plt $plt $beam &> $output
   diff -u $expected $output
 done
