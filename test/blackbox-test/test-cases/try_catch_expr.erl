505180cc08191221c3de4962bcf2054c9434154e
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2019-06-06 19:16:56 +0900

    add tests

--- test/blackbox-test/test-cases/try_catch_expr.erl
+++ test/blackbox-test/test-cases/try_catch_expr.erl
@@ -1,8 +1,34 @@
 -module(try_catch_expr).
 
--export([main/0]).
+-export([catch_expr/0, expr_try1/0, expr_try2/0, expr_try3/0]).
 
--spec main() -> any().
-main() ->
+-spec catch_expr() -> any().
+catch_expr() ->
     catch 1 + 2.
-    
+
+
+-spec expr_try1() -> number().
+expr_try1() ->
+    try
+        1 + 2
+    catch
+        Err:Stack -> {hoge, Err, Stack}
+    end.
+
+-spec expr_try2() -> number().
+expr_try2() ->
+    try 1 + 2 of
+        X -> 2 * X
+    catch
+        Err:Stack -> {hoge, Err, Stack}
+    end.
+
+-spec expr_try3() -> ok.
+expr_try3() ->
+    try 1 + 2 of
+        X -> 2 * X
+    catch
+        Err:Stack -> {hoge, Err, Stack}
+    after
+        ok
+    end.
