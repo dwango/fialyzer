6888b7b35937cbf3d4a358083f400e0a3feaaffb
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2019-08-20 18:11:26 +0900

    add test

--- test/blackbox-test/test-cases/case_expr.erl
+++ test/blackbox-test/test-cases/case_expr.erl
@@ -1,10 +1,17 @@
 -module(case_expr).
 
--export([main/1]).
+-export([main/1, three_tuple/1]).
 
 -spec main(1 | 'ok') -> 'foo' | 123.
 main(X) ->
     case X of
         1 -> 'foo';
         ok -> 123
+    end.
+
+%% three tuple pattern: https://github.com/yutopp/obeam/issues/101
+-spec three_tuple({abc, integer(), atom()}) -> any().
+three_tuple(X) ->
+    case X of
+        {abc, Foo, Bar} -> {ok, Foo, Bar}
     end.
